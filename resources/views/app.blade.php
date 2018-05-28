3edbd7efc330b7fa0ea895ccbb5d3a7fd0226a01
ralphdev <rafa.developers@gmail.com>
2018-05-28 10:18:06 -0500

    se procedio hacer la rutas REST para empezar con el proyecto y se ejecutaron las migraciones

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,22 +15,7 @@
     <body>
 
         <div id="main" class="container">
-            <div class="row">
-                <div class="col-sm-4">
-                    <h1>VUEjs - AJAX axios</h1>
-                    <ul class="list-group">
-                        <li v-for="item in lists" class="list-group-item">                      
-                            @{{ item.name }}
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-sm-8">
-                    <h1>JSON</h1>
-                    <pre>
-                        @{{ $data | json }}
-                    </pre>
-                </div>
-            </div>
+            @yield('content')
         </div>
         
         <script src="{{ asset('js/app.js') }}"></script>
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('app')
+
+@section('content')
+	
+	<h1>desde el dashboard</h1>
+@endsection
